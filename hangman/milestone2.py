7774c88bf040b1be4cd1bbace7fce5a1c44df184
azlan <azlanrajj@gmail.com>
2023-09-29 12:49:42 +0100

    Adding milestone2 to main

---
+++ hangman/milestone2.py
@@ -0,0 +1,8 @@
+# Step 1:
+# Create a list containing the names of your 5 favorite fruits.
+# Step 2:
+# Assign this list to a variable called word_list.
+# Step 3:
+# Print out the newly created list to the standard output (screen).
+word_list = ["banana", "mango", "orange", "pineapple", "watermellon"]
+print(word_list)
