bef6537c224f6228b562a57a5f918943cdcdbbec
azlan <azlanrajj@gmail.com>
2023-09-29 13:27:39 +0100

    updated milestone2

--- hangman/milestone2.py
+++ hangman/milestone2.py
@@ -55,3 +55,19 @@ else:
 # File structure of the project
 # License information
 # You don't have to write all of this at once, but make sure to update your README file as you go along, so that you don't forget to add anything.
+
+# MILESTONE 3
+# Write code that will continuously ask the user for a letter and validate it.
+# Create a new script called milestone_3.py. This file will contain the code for this milestone.
+# Step 1:
+# Create a while loop and set the condition to True. Setting the condition to True` ensures that the code runs continuously.
+# In the body of the loop, write the code required for the following steps.
+# Step 2:
+# Ask the user to guess a letter and assign this to a variable called guess.
+# Step 3:
+# Check that the guess is a single alphabetical character. Hint: You can use Python's isalpha method to check if the guess is alphabetical.
+# Step 4:
+# If the guess passes the checks, break out of the loop.
+# Step 5:
+# If the guess does not pass the checks, then print a message saying "Invalid letter. Please, enter a single alphabetical character."
+
